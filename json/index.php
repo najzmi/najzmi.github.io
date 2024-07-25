561f2df04ec3d0ec54aa35d387c950e05c049390
uptime.web.id <158476039+najzmi@users.noreply.github.com>
2024-07-25 14:30:30 +0700

    Create index.php

---
+++ json/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Diam Itu Emas</title>
+  </head>
+  <body>
+    <h1>Diam Itu Emas</h1>
+  </body>
+</html>
