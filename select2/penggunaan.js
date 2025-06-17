bc3a961efaafcf52f81c011c7e211d173a48ff18
italazhar.com <158476039+najzmi@users.noreply.github.com>
2025-06-17 11:00:10 +0700

    Add files via upload

---
+++ select2/penggunaan.js
@@ -0,0 +1,9 @@
+/*-- Select 2 --*/
+/*-- https://github.com/ttskch/select2-bootstrap4-theme **/
+	$('#select1').select2({
+		theme: 'bootstrap4',
+	});
+	$('#select2').select2({
+		theme: 'bootstrap4',
+	});
+	
